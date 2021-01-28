517c217d0c10d3588bb44f3e5d8a41bfa017647b
sieun-Bae <qotldms130@gmail.com>
2021-01-29 00:34:55 +0900

    bfs, bruteforce, 재귀

--- 14502.cpp
+++ 14502.cpp
@@ -4,20 +4,28 @@
 #include <queue>
 using namespace std;
 
-int n, m;
-int map[9][9];
-bool visited[9][9];
+int n,m,answer;
+int map[8][8];
+int temp_map[8][8];
+
+//bool visited[9][9];
 int dx[4] = {-1,1,0,0}, dy[4] = {0,0,-1,1};
-queue< pair<int,int> > q;
+vector< pair<int,int> > virus;
 
 void bfs()
 {
+	queue< pair<int,int> > q;
+	for (int i=0;i<virus.size();i++) {
+		q.push(make_pair(virus[i].first, virus[i].second));
+	}
+
 	while (!q.empty()) {
 		//체크아웃
 		int x = q.front().first;
 		int y = q.front().second;
 		q.pop();
-		//목적지인가?
+
+		//목적지인가? => 완전탐색
 
 		//갈 수 있는가?
 		for (int i=0;i<4;i++) {
@@ -25,25 +33,45 @@ void bfs()
 			int ny = y+dy[i];
 
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (map[nx][ny] == 1) continue;
-			if (map[nx][ny] == 0) {
+			//if (temp_map[nx][ny] == 1) continue;
+			if (temp_map[nx][ny] == 0) {
 				//간다
 				//체크인&큐에넣기
-
-				map[nx][ny] = 2;
+				temp_map[nx][ny] = 2;
 				q.push(make_pair(nx, ny));
 			}
 		}
 	}
 
-}
-void printmap()
-{
+	int temp_answer = 0;
 	for (int i=0;i<n;i++) {
-		for (int j=0;j<n;j++) {
-			printf("%d",map[i][j]);
+		for (int j=0;j<m;j++) {
+			if (temp_map[i][j] == 0)
+				temp_answer++;
 		}
-		printf("\n");
+	}
+	answer = max(answer, temp_answer);
+}
+
+void wall(int cnt)
+{
+	if (cnt == 3) {
+		for (int i=0;i<n;i++) {
+			for (int j=0;j<m;j++) {
+				temp_map[i][j] = map[i][j];
+			}
+		}
+
+		return bfs();
+	}
+	for (int i=0;i<n;i++) {
+		for (int j=0;j<m;j++) {
+			if (map[i][j] == 0) {
+				map[i][j] = 1;
+				wall(cnt+1);
+				map[i][j] = 0;
+			}
+		}
 	}
 }
 
@@ -54,16 +82,22 @@ int main()
 		for (int j=0;j<m;j++) {
 			scanf("%d", &map[i][j]);
 			if (map[i][j] == 2) {
-				q.push(make_pair(i, j));
+				virus.push_back(make_pair(i, j));
 			}
 		}
 	}
-	printmap();
 
-	bfs();
+	for (int i=0;i<n;i++) {
+		for (int j=0;j<m;j++) {
+			if (map[i][j] == 0) {
+				map[i][j] = 1;
+				wall(1);
+				map[i][j] = 0;
+			}
+		}
+	}
 
-	printmap();
-
+	printf("%d", answer);
 
 	return 0;
 }
