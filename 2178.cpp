f0d94bcaaab18dd15ca817f4605ea97a67b3b71d
sieun-Bae <qotldms130@gmail.com>
2021-03-15 02:01:24 +0900

    bfs, memory, time issue

--- 2178.cpp
+++ 2178.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
 #define MAX 987654321
 using namespace std;
 
-int map[110][110], n, m;
+int n, m;
 bool visited[110][110];
-int result = 0;
+char map[110][110]; 
+int result = MAX;
+int check[110][110];
 int dx[4] = {0,0,1,-1};
 int dy[4] = {1,-1,0,0};
 
-void bfs(int x, int y)
+int bfs(int x, int y, int cnt)
 {
 	visited[x][y] = true;
-	queue<pair <int,int> > q;
-	q.push(make_pair(x,y));
+	queue< pair < pair <int,int>, int > > q;
+	q.push(make_pair(make_pair(x,y),cnt));
 	while(!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
+		x = q.front().first.first;
+		y = q.front().first.second;
+		cnt = q.front().second;
+		q.pop();
+
+		if (x==(n-1) && y==(m-1)) return cnt;
 		for (int i=0;i<4;i++) {
 			int tx = x+dx[i];
 			int ty = y+dy[i];
 			if (tx < 0 || tx >= n || ty < 0 || ty >= m) continue;
-			if (map[tx][ty] == 1 && visited[tx][ty] == false) {
+			if (map[tx][ty] == '1' && visited[tx][ty] == false) {
 				//간다
-				result++;
 				visited[x][y] = true;
-				q.push(make_pair(tx, ty));
+				q.push(make_pair(make_pair(tx, ty),cnt+1));
 			}
 		}
 	}
@@ -33,7 +39,6 @@ void bfs(int x, int y)
 void dfs(int x, int y, int cnt)
 {
 	//체크인
-	
 	//목적지인가?
 	if (x==(n-1) && y==(m-1)) {
 		result = min(result, cnt);
@@ -45,7 +50,7 @@ void dfs(int x, int y, int cnt)
 		int ty = y+dy[i];
 		//갈수있는가?
 		if (tx < 0 || tx >= n || ty < 0 || ty >= m) continue;
-		if (map[tx][ty] == 1 && visited[tx][ty] == false) {
+		if (map[tx][ty] == '1' && visited[tx][ty] == false) {
 			//간다
 			visited[x][y] = true;
 			dfs(tx, ty, cnt+1);
@@ -59,12 +64,9 @@ int main()
 {
 	scanf("%d%d",&n,&m);
 	for (int i=0;i<n;i++) {
-		for (int j=0;j<m;j++) {
-			scanf("%1d",&map[i][j]);
-		}
+		scanf("%s", map[i]);
 	}
-	
-	dfs(0,0,1);
-	printf("%d\n", result);
+	//dfs(0,0,1);
+	printf("%d\n", bfs(0,0,1));
 	return 0;
 }
