44199739ef622d316baf47aaffcf51f046c860a1
sieun-Bae <qotldms130@gmail.com>
2021-04-01 21:04:36 +0900

    simulation

---
+++ 19236.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef struct Info {
+	int num;
+	int x,y;
+	int dir;
+	Info(int nn, int xx, int yy, int dd) {
+		num = nn;
+		x = xx;
+		y = yy;
+		dir = dd;
+	}
+	Info () {};
+}info;
+
+vector <info> ocean[4]; //(num, dir)
+vector <info> fish; //(x, y)
+int visited[4][4];
+info shark;
+
+bool comp(const info& o1, const info& o2) {
+	return o1.num < o2.num;
+}
+/*
+void dfs()
+{
+	// 상어 이동
+		//먹은 물고기의 방향으로 선택적으로 이동, 물고기 먹으면 멈춤
+		//물고기 없는 칸으로는 이동 불가, 이동할 수 있는 칸 없으면 종료
+	if (상어 이동 불가) {
+
+		return;
+	}
+	//주변 탐색
+	for (int i=0;i<4;i++) {
+
+	}
+	// 물고기 이동
+		//1번 물고기부터 이동, 이동 방향에 물고기 있으면 위치 교환
+		//이동할 수 없으면 45도 반시계 회전,  
+	// 
+}
+*/
+int main()
+{
+	for (int i=0;i<4;i++) {
+		for (int j=0;j<4;j++) {
+			int num, dir;
+			scanf("%d%d", &num, &dir);
+			ocean[i].push_back(Info(num, i, j, dir));
+			fish.push_back(Info(num, i, j, dir));
+		}
+	}
+
+	sort(fish.begin(), fish.end(), comp);
+	shark = Info(-1, 0, 0, ocean[0][0].dir);
+
+	
+	
+	for (int i=0;i<16;i++) {
+		printf("num: %d, x: %d, y: %d\n", fish[i].num, fish[i].x, fish[i].y);
+	}
+	
+	return 0;
+}
